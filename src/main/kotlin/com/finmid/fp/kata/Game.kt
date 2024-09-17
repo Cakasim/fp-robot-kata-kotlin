01860ffaf5dbfeecb6898dce097f05f978437630
David <david@finmid.com>
2024-09-17 15:36:33 +0200

    Next test to pass

--- src/main/kotlin/com/finmid/fp/kata/Game.kt
+++ src/main/kotlin/com/finmid/fp/kata/Game.kt
@@ -18,7 +18,7 @@ private fun Map.moveNextPosition(direction: Direction) = when (direction) {
     LEFT -> robot.tryMoveLeft()
     RIGHT -> robot.tryMoveRight(width)
     DOWN -> robot.tryMoveDown(height)
-    UP -> robot.moveUp()
+    UP -> robot.tryMoveUp()
 }
 
 private fun Position.moveRight() = Position(x + 1, y)
@@ -29,7 +29,9 @@ private fun Position.moveUp() = Position(x, y - 1)
 private fun Position.canMoveLeft() = x > 0
 private fun Position.canMoveRight(width: Int) = x < width - 1
 private fun Position.canMoveDown(height: Int) = y < height - 1
+private fun Position.canMoveUp() = y > 0
 
 private fun Position.tryMoveLeft() = if (canMoveLeft()) moveLeft() else this
 private fun Position.tryMoveRight(width: Int) = if (canMoveRight(width)) moveRight() else this
 private fun Position.tryMoveDown(height: Int) = if (canMoveDown(height)) moveDown() else this
+private fun Position.tryMoveUp() = if (canMoveUp()) moveUp() else this
